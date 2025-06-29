bac24716972e828fd6c1a0e59c104aa4d6d1d4ff
Chang Michael <xcchenmichael@gmail.com>
2025-06-28 20:22:07 -0400

    Basic add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int firstNum, secondNum;
+    cout << "Enter first number: ";
+    cin >> firstNum;
+    cout << "Enter second number: ";
+    cin >> secondNum;
+    cout << firstNum << " + " << secondNum << " = " << firstNum + secondNum << endl;
 }
