7caf6ff0b7ca1ec473396c7e4f62b4951aa19b8f
Chang Michael <xcchenmichael@gmail.com>
2025-06-28 21:18:44 -0400

    One line operation without precedence

--- main.cpp
+++ main.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <stack>
 
 using namespace std;
 
-int main() {
-    int firstNum, secondNum;
-    cout << "Enter first number: ";
-    cin >> firstNum;
-    cout << "Enter second number: ";
-    cin >> secondNum;
-    cout << firstNum << " + " << secondNum << " = " << firstNum + secondNum << endl;
+
+vector<string> parser(string input) {
+    vector<string> res;
+    istringstream iss(input);
+    string token;
+    while (iss >> token) {
+        res.push_back(token);
+        //cout<< res.back() << endl;
+    }
+    return res;
 }
+
+int main() {
+    string input;
+    cout << "Please enter the expression(use space to separate the operator and number): "<<endl;
+    getline(cin, input);
+    //cout << input << endl;
+    vector<string> res = parser(input);
+
+    string op = "";
+    int number = -1;
+    for (string token : res) {
+        if (token == "+" || token == "-" || token == "*" || token == "/") {
+            if (op != "") {
+                cerr << "Operator already exists" << endl;
+            }
+            op = token;
+            continue;
+        }
+        try {
+            int tmp = stoi(token);
+            if (number == -1) {
+                number = tmp;
+                continue;
+            }
+            if (op != "") {
+                if (op == "+") {
+                    number += tmp;
+                }else if (op == "-") {
+                    number -= tmp;
+                }else if (op == "*") {
+                    number *= tmp;
+                }else if (op == "/"){
+                    number /= tmp;
+                }
+                op = "";
+            }
+        }catch (const exception& e) {
+            cerr << e.what() << endl;
+        }
+
+    }
+
+    cout << number << endl;
+}
+
+
